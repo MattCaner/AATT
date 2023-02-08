52a2c30da3981aa40362a136935b28afb57a8bc0
Maciej Trzciński <maciejftrzcinski@gmail.com>
2023-02-08 15:53:00 +0100

    Updated evaluation in just training

--- just_train.py
+++ just_train.py
@@ -2,8 +2,12 @@
 import transformer as t
 import torch
 from pprint import pprint
+import os
 
-general_params = t.ParameterProvider("params_local.config")
+#configpath = os.environ["SCRATCH"] + "/params2.config"
+configpath = "params_local.config"
+
+general_params = t.ParameterProvider(configpath)
 
 v_in = t.VocabProvider(general_params,general_params.provide("language_in_file"))
 v_out = t.VocabProvider(general_params,general_params.provide("language_out_file"))
@@ -14,48 +18,32 @@ transformer = t.Transformer(general_params,v_in,v_out)
 
 lr = 0.001
 
-for i in range(0,1):
+for i in range(0,1000):
     print("iteration: "+str(i)+"lr: "+str(lr))
     t.train_cuda(transformer, train_dataset, torch.cuda.current_device(), batch_size = 32, lr = lr, epochs = 20)
-    print("Evaluation: ")
+    print("Evaluation: ", flush=True)
     quality = t.evaluate(transformer,test_dataset,use_cuda = True, device = torch.cuda.current_device(),batch_size=32)
-    print(quality)
+    print(quality,flush=True)
 
 q = t.evaluate(transformer,test_dataset,use_cuda = True, device = torch.cuda.current_device(),batch_size=32)
 
 bleu = t.calculate_bleu(transformer,train_dataset)
 rogue = t.calculate_rogue(transformer,train_dataset)
 
+print("----------------------------", flush=True)
 
-print(bleu)
-pprint(rogue)
+f_to_translate = open(general_params.provide("language_in_file"),'r',encoding = 'utf-8')
+f_to_compare = open(general_params.provide("language_out_file"),'r',encoding = 'utf-8')
+toReturn =  t.raw_data_bleu(transformer,f_to_translate,f_to_compare)
+f_to_translate.close()
+f_to_compare.close()
+print(toReturn)
 
-f = open('simplepl.txt','r',encoding = 'utf-8')
-f2 = open('simpleen.txt','r',encoding = 'utf-8')
+print("---------------------------", flush = True)
 
-lines_compare = list(map(str.lower,f2.readlines()))
-lines = list(map(str.lower,f.readlines()))
-
-
-for i, line in enumerate(lines):
-    print(transformer.processSentence(line,autoprint=True))
-    print(line)
-    print(lines_compare[i])
-    print("-----------------------")
-
-f.seek(0)
-f2.seek(0)
-
-bleu = t.raw_data_bleu(transformer,f,f2)
-
-
-f.seek(0)
-f2.seek(0)
-
-rogue = t.raw_data_rogue(transformer,f,f2)
-
-
-print(bleu)
-pprint(rogue)
-
-
+f_to_translate = open(general_params.provide("language_in_file"),'r',encoding = 'utf-8')
+f_to_compare = open(general_params.provide("language_out_file"),'r',encoding = 'utf-8')
+toReturn =  t.raw_data_rogue(transformer,f_to_translate,f_to_compare)
+f_to_translate.close()
+f_to_compare.close()
+pprint(toReturn)
